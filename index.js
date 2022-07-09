8ddb0e93506175d8a56b03d4c85976f95bc8cf9c
Vlad Feninets <v.feninets@gmail.com>
2022-07-09 14:01:54 +0300

    add checks and show errors, change JS converter, add IOS converter

--- index.js
+++ index.js
@@ -30,6 +30,10 @@ async function run() {
             await converterAndroid(db, collections)
         }
 
+        if (PLATFORM === 'ios') {
+            await converterIos(db, collections)
+        }
+
         process.exit(0)
     }
     catch (e) {
@@ -49,28 +53,83 @@ function removeId({ _id, ...rest } = {}) {
     return rest
 }
 
+function getTargetLangDoc(collectionName, docs, lang) {
+    const targetDoc = docs.filter(doc => {
+        if (!doc.lang) {
+            console.error('🚩 Document should have a `lang` property\n', collectionName, ':', JSON.stringify(doc))
+            process.exit(1)
+        }
+        return doc.lang === lang
+    }).pop()
+
+    if (!targetDoc) {
+        console.error('🚩 Document is empty or doesn\'t have a second lang\n', collectionName, ':', JSON.stringify(docs))
+        process.exit(1)
+    }
+
+    const { lang: _lang, ...rest } = targetDoc // exclude a lang property from the result
+    return rest
+}
+
+function checkThatDiffLangDocumentsHaveSamePropsCount(doc1, doc2) {
+    const countTheSame = Object.keys(doc1 ?? {}).length === Object.keys(doc2 ?? {}).length
+
+    if (!countTheSame) {
+        console.error('🚩 Document should have the same count of properties for different languages\n', JSON.stringify(doc1))
+        process.exit(1)
+    }
+}
+
 async function converterJs(db, collections) {
     return new Promise((resolve) => {
-        const _writeToDisk = async (lang, collectionName, content) => {
-            const fileName = `./localization-artifacts/${collectionName}-${lang}.json`
+        // IF WE NEED TO STORE AN EACH COLLECTION IF A SEPARATE FILE
+        // const _writeToDisk = async (lang, collectionName, content) => {
+        //     const fileName = `./localization-artifacts/${collectionName}-${lang}.json`
+        //     await fs.writeFile(fileName, content)
+        //     console.log(`✅ Updated - ${fileName}`);
+        // }
+
+        const _writeToDisk = async (lang, content) => {
+            const fileName = `./localization-artifacts/localization-${lang}.json`
             await fs.writeFile(fileName, content)
             console.log(`✅ Updated - ${fileName}`);
         }
 
         let counter = 0
+        let enContent = []
+        let ukContent = []
 
         collections.forEach(async (collection) => {
             const docs = await db.collection(collection.name).find({}).toArray()
             if (docs.length) {
-                const [en, uk] = docs
-                _writeToDisk('en', collection.name, JSON.stringify(removeId(en), null, 2))
-                _writeToDisk('uk', collection.name, JSON.stringify(removeId(uk), null, 2))
+                const en = getTargetLangDoc(collection.name, docs, 'en')
+                const uk = getTargetLangDoc(collection.name, docs, 'ua')
+                checkThatDiffLangDocumentsHaveSamePropsCount(en, uk)
+
+                const getData = (doc) => Object.keys(doc).filter(key => key !== '_id').map(key => {
+                    return { key: `${collection.name}_${key}`, value: doc[key] }
+                })
+
+                enContent.push(...getData(en))
+                ukContent.push(...getData(uk))
+
+                // IF WE NEED TO STORE AN EACH COLLECTION IF A SEPARATE FILE
+                // _writeToDisk('en', collection.name, JSON.stringify(removeId(en), null, 2))
+                // _writeToDisk('uk', collection.name, JSON.stringify(removeId(uk), null, 2))
                 counter++
             } else {
                 counter++
             }
 
             if (counter === collections.length) {
+                const prepareContent = langDoc => JSON.stringify(langDoc.reduce((acc, { key, value }) => {
+                    acc = { ...acc, [key]: value }
+                    return acc
+                }, {}), null, 2)
+
+                await _writeToDisk('en', prepareContent(enContent))
+                await _writeToDisk('uk', prepareContent(ukContent))
+
                 console.log(`\n\n😈 Complete!`);
                 resolve()
             }
@@ -95,7 +154,11 @@ async function converterAndroid(db, collections) {
             const docs = await db.collection(collection.name).find({}).toArray()
 
             if (docs.length) {
-                const [en, uk] = docs
+                const en = getTargetLangDoc(collection.name, docs, 'en')
+                const uk = getTargetLangDoc(collection.name, docs, 'ua')
+
+                checkThatDiffLangDocumentsHaveSamePropsCount(en, uk)
+
                 enContent.push(_convertJsonToXML(collection.name, removeId(en)))
                 ukContent.push(_convertJsonToXML(collection.name, removeId(uk)))
                 counter++
@@ -113,6 +176,52 @@ async function converterAndroid(db, collections) {
                 await fs.writeFile(enFileName, [prefix, ...enContent, postfix].join('\n'))
                 console.log(`✅ Updated - ${enFileName}`);
                 await fs.writeFile(ukFileName, [prefix, ...ukContent, postfix].join('\n'))
+                console.log(`✅ Updated - ${ukFileName}`);
+                console.log(`\n\n😈 Complete!`);
+                resolve()
+            }
+        })
+    })
+}
+
+async function converterIos(db, collections) {
+    return new Promise((resolve) => {
+        const _convertJsonToIosFormat = (collectionName, data) => {
+            return Object.keys(data).map(key => {
+                return `"${collectionName}_${key}" = ${data[key]}`
+            }).join('\n')
+        }
+
+        let counter = 0
+
+        let enContent = []
+        let ukContent = []
+
+        collections.forEach(async (collection, index) => {
+            const docs = await db.collection(collection.name).find({}).toArray()
+
+            if (docs.length) {
+                const en = getTargetLangDoc(collection.name, docs, 'en')
+                const uk = getTargetLangDoc(collection.name, docs, 'ua')
+
+                checkThatDiffLangDocumentsHaveSamePropsCount(en, uk)
+
+                enContent.push(_convertJsonToIosFormat(collection.name, removeId(en)))
+                ukContent.push(_convertJsonToIosFormat(collection.name, removeId(uk)))
+                counter++
+            } else {
+                counter++
+            }
+
+            if (counter === collections.length) {
+                const prefix = `/*\n\tLocalizable.strings\n\tDucky\n\tCreated by https://hub.docker.com/r/fnnzzz/ducky-localize\n\ton ${new Date().toJSON()}\n*/\n\n`
+
+                const enFileName = './localization-artifacts/Localizable_EN.strings'
+                const ukFileName = `./localization-artifacts/Localizable_UK.strings`
+
+                await fs.writeFile(enFileName, [prefix, ...enContent].join('\n'))
+                console.log(`✅ Updated - ${enFileName}`);
+                await fs.writeFile(ukFileName, [prefix, ...ukContent].join('\n'))
                 console.log(`✅ Updated - ${ukFileName}`);
                 console.log(`\n\n😈 Complete!`);
                 resolve()
